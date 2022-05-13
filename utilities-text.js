71bd97db944e0cf8b1ec931597c18e146f78e8e6
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-05-13 12:55:16 +0200

    update

--- utilities-text.js
+++ utilities-text.js
@@ -1,35 +1,3 @@
-function getConsoleArguments() {
-  return process.argv.slice(2);
-}
-
-function getArgumentOrExitWithErrorAndIndex(errorString, index) {
-  const arguments = getConsoleArguments()
-  let arg;
-  if (arguments[index]){
-    arg = arguments[index];
-  } else {
-    console.error(errorString);
-    process.exit();
-  }
-  return arg;
-}
-
-function getOptionalArgumentWithIndex(index) {
-  const arguments = getConsoleArguments();
-  return arguments[index];
-}
-
-function readFileDataWithUrl(inputUrl){
-  let fileData;
-  try {
-    fileData = fs.readFileSync(inputUrl, 'utf8');
-  } catch (error) {
-    console.log('errore nella lettura del file\n', error.message);
-    process.exit();
-  }
-  return fileData;
-}
-
 function getCharNumber(string) {
   return string.length;
 }
@@ -81,37 +49,8 @@ function createReportString(originalText, searchWord, charNumber, noSpacesCharNu
                     occurrenceString;
 return newFileData;
 }
-
-function writeReportInFile(outputUrl, report) {
-  try {
-    fs.writeFileSync(outputUrl, report);
-  } catch (error) {
-    console.log('errore nella scrittura del file\n', error.message);
-    process.exit();
-  }
-}
-
-
-const fs = require('fs');
-
-const inputUrl = getArgumentOrExitWithErrorAndIndex("devi inserire l'input url", 0)
-const outputUrl = getArgumentOrExitWithErrorAndIndex("devi inserire l'output url", 1)
-
-let searchWord = getOptionalArgumentWithIndex(2)
-
-let fileData = readFileDataWithUrl(inputUrl);
-
-const charNumber = getCharNumber(fileData);
-
-const noSpacesCharNumber = getCharNumberWithoutSpaces(fileData);
-
-const wordNumber = getWordNumber(fileData);
-
-let occurence = -1;
-if (searchWord) {
-  occurence = getOccurrenciesWordInString(searchWord, fileData)
-}
-
-const report = createReportString(fileData,searchWord,charNumber,noSpacesCharNumber,wordNumber,occurence)
-
-writeReportInFile(outputUrl, report)
+  exports.getCharNumber = getCharNumber;
+  exports.getCharNumberWithoutSpaces = getCharNumberWithoutSpaces;
+  exports.getWordNumber = getWordNumber;
+  exports.getOccurrenciesWordInString = getOccurrenciesWordInString;
+  exports.createReportString = createReportString;
